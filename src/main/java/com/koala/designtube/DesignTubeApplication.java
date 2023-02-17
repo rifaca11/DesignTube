e1785924cb484a3ae9515fa20d1f9b18869cd61d
rifaca11 <charifah1112@gmail.com>
2023-02-17 11:13:31 +0100

    first commit

---
+++ src/main/java/com/koala/designtube/DesignTubeApplication.java
@@ -0,0 +1,13 @@
+package com.koala.designtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignTubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignTubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koala/designtube/DesignTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.designtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignTubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
